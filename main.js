2459874545b9ce185978ca9694706a7f1655d8f5
Paulo Santana <paulosantana.sa@gmail.com>
2022-03-11 17:48:05 -0300

    aluraflix newversion. no array and exercise functions.

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
-let movieList = [
-"https://upload.wikimedia.org/wikipedia/pt/9/9a/Big-bang-theory_Temporada_12_poster.jpg",
-"https://imania.com.br/wp-content/uploads/2020/10/harry-potter-7.png",
-"https://s2.glbimg.com/QSYSNn6iOxdy_B4xRufk5EIODEc=/362x536/https://s2.glbimg.com/n3U3-nxkWddQ051zh597_NU-ip8=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2021/X/F/v6OfUvQrqybWyzDYu0KA/2021-1682-how-i-met-your-mother-poster.jpg",
-"https://br.web.img3.acsta.net/medias/nmedia/18/90/95/85/20122112.jpg",
-"https://br.web.img2.acsta.net/pictures/19/09/15/23/52/1729197.jpg"
-];
+// let movieList = [
+// "https://upload.wikimedia.org/wikipedia/pt/9/9a/Big-bang-theory_Temporada_12_poster.jpg",
+// "https://imania.com.br/wp-content/uploads/2020/10/harry-potter-7.png",
+// "https://s2.glbimg.com/QSYSNn6iOxdy_B4xRufk5EIODEc=/362x536/https://s2.glbimg.com/n3U3-nxkWddQ051zh597_NU-ip8=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2021/X/F/v6OfUvQrqybWyzDYu0KA/2021-1682-how-i-met-your-mother-poster.jpg",
+// "https://br.web.img3.acsta.net/medias/nmedia/18/90/95/85/20122112.jpg",
+// "https://br.web.img2.acsta.net/pictures/19/09/15/23/52/1729197.jpg"
+// ];
 
-let newMovie = document.querySelector("#showMovieList");
-let i;
+// let newMovie = document.querySelector("#showMovieList");
+// let i;
 
-for(i = 0; i < movieList.length; i++) {
-    document.write("<img src="+ movieList[i] +">")
-}
+// for(i = 0; i < movieList.length; i++) {
+//     document.write("<img src="+ movieList[i] +">")
+// }
 
-function addMovie(){
-    let imgLink = document.querySelector("#imgLink").value;
+// function addMovie(){
+//     let imgLink = document.querySelector("#imgLink").value;
     
-    if ( movieList.indexOf(imgLink) === -1){
+//     if ( movieList.indexOf(imgLink) === -1){
     
-        movieList.push(imgLink);
+//         movieList.push(imgLink);
 
-        newMovie.innerHTML = newMovie.innerHTML + "<img src=" +imgLink+ ">" 
+//         newMovie.innerHTML = newMovie.innerHTML + "<img src=" +imgLink+ ">" 
+//     } else {
+//         alert("Filme já adicionado! Por favor selecione um Título diferente.");
+//     }
+// }
+
+
+function movieAdd () {
+    let movieId = document.getElementById('movieId').value;
+    
+    if (movieId.endsWith(".jpg")) {
+        showMovieList(movieId);
     } else {
-        alert("Filme já adicionado! Por favor selecione um Título diferente.");
+        console.error("Endereço do filme inválido");
     }
+    
+    movieId = "";
 }
 
+function showMovieList(movieId) {
+    let movieElement = "<img src=" + movieId + ">";
+    let movieList = document.getElementById('movieList');
+    
+    movieList.innerHTML = movieList.innerHTML + movieElement;
+}
 
 
